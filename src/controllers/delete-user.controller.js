8ffe30366973b07a0e3fc42bd5eceb7a9e00106e
Sijal Manandhar <sijal.m06@gmail.com>
2024-11-03 17:04:45 +0545

    delete notes when deleting user

--- src/controllers/delete-user.controller.js
+++ src/controllers/delete-user.controller.js
@@ -1,7 +1,10 @@
 const User = require("../models/user.model");
+const Note = require("../models/note.model");
 
 const deleteUserController = async (req, res) => {
+  await Note.deleteMany({ author: req.userId });
   await User.findByIdAndDelete(req.userId);
+  res.clearCookie("refreshToken");
   res.sendStatus(204);
 };
 
